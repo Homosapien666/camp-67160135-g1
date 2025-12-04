9f1f1a6ea63c6240101fce393149fa59144cb86b
Homosapien666 <wanuttapongw999@gmail.com>
2025-12-04 19:04:05 +0700

    101

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,35 @@
+<!-- file resources/views/html101.blade.php -->
+
+<!Doctype html>
+
+<html>
+    <head>
+        <title>ส่วนหัวของ HTML</title>
+        <link rel = "stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <style>
+            h1 {font-size : 50px ;
+                color :red;}
+         </style>   
+        <h1>HTML 101</h1>
+        <h1>&lt;HTML 101</h1>
+        <h2>HTML 101</h2>
+        <h3>&gt;HTML 101</h3>
+        <hr />
+        <hr>
+        <table border ="1" width="50%">
+            <thead>
+                <tr>
+                    <th>ลำดับ</th>
+                    <td>ชื่อ</td>
+                <tr>
+                    <td>1</td>
+                    <td>Wanuttapong</td>
+                </tr>
+            </tbody>
+            <thead>
+                <tr>
+
+    </body>
+</html>
